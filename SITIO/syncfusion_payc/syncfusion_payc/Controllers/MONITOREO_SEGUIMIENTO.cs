7cd52366e2dc6171076f8d32fb57a237d58e3e4f
DIRECTOR ANALITICA <director.analitica@payc.com.co>
2019-08-14 15:00:16 -0500

    Commit con el sitio final

---
+++ SITIO/syncfusion_payc/syncfusion_payc/Controllers/MONITOREO_SEGUIMIENTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace syncfusion_payc.Controllers
+{
+    public class MONITOREO_SEGUIMIENTOController : Controller
+    {
+        // GET: PROYECCIONVARIABLES
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
