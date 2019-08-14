65ccee5eec9b603e3344c4fbfce22a59ddd16af4
DIRECTOR ANALITICA <director.analitica@payc.com.co>
2019-08-13 22:11:24 -0500

    Creacion procedimiento actualizacion indicadores en sitio

---
+++ SITIO/syncfusion_payc/syncfusion_payc/Controllers/ACTUALIZACION_REPORTEADOR.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace syncfusion_payc.Controllers
+{
+    public class ACTUALIZACION_REPORTEADORController : Controller
+    {
+        // GET: PROYECCIONVARIABLES
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
